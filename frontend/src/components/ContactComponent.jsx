220e3b328b3e5d636674a4cc584ef25db69bd264
raJiB <pokhrelrajib016@gmail.com>
2024-03-11 20:49:32 +0545

    chnaged contact layout

--- frontend/src/components/ContactComponent.jsx
+++ frontend/src/components/ContactComponent.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import Header from '../Header'
+import Footer from '../Footer'
 export default function ContactComponent() {
   return (
     <div>
+      <Header />
         <h1>Contact</h1>
         <p>This is the contact page</p>
-      
+      <Footer />
     </div>
   )
 }
